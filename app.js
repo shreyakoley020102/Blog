a1c612d24955ca728b6c5aa3b277875b83e05a6f
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-08-01 18:46:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -49,10 +49,10 @@ res.redirect("/");
 })
 
 app.get("/posts/:postName", function(req,res){
- const requestedTitle=req.params.postName;
+ const requestedTitle = _.lowerCase(req.params.postName);
 
  posts.forEach(function(post){
-   const storedTitle=post.title;
+   const storedTitle=_.lowerCase(post.title);
 
    if(storedTitle===requestedTitle){
      console.log("Match found!");
